c948f02f63a8f9c2cfe62fb79f14f0224c4f0672
cleandevcode <freelance1331@gmail.com>
2021-08-05 01:24:33 +0800

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { Navigation } from "./components/navigation";
-import HomePage  from './pages/Home/home';
-import LoginPage from './pages/Login/login';
-import RegisterPage from './pages/Login/register';
+import HomePage from "./pages/Home/home";
+import LoginPage from "./pages/Login/login";
+import RegisterPage from "./pages/Login/register";
 import ForgotPasswordPage from "./pages/Login/forgotPassword";
 import ProfilePage from "./pages/Profile/profile";
 import LinkWalletPage from "./pages/Wallet/link";
-import { Footer } from './components/footer';
-import { history, Routes }  from './routes';
-
+import { Footer } from "./components/footer";
+import { history, Routes } from "./routes";
 
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
@@ -29,7 +28,7 @@ const App = () => {
   return (
     <div>
       <Navigation />
-      <Router history={history}>
+      {/* <Router history={history}>
         <Switch>
           <Route exact path={Routes.Home.path} component={HomePage} />
           <Route exact path={Routes.Login.path} component={LoginPage} />
@@ -40,7 +39,9 @@ const App = () => {
           <Route exact path={Routes.Earning} component={LinkWalletPage} />
           <Redirect from="*" to="/" />
         </Switch>
-      </Router>
+      </Router> */}
+      <ProfilePage />
+
       <Footer />
     </div>
   );
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,18 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { Welcome } from './welcome';
-import { Job } from './job';
-import { Subscribe } from './subscribe';
+import { useSelector, useDispatch } from "react-redux";
+import { Welcome } from "./welcome";
+import { Job } from "./job";
+import { Subscribe } from "./subscribe";
 
 const HomePage = ({ data }) => {
-    const isAuthed = useSelector((state) => state.auth.isAuthed);
+  const isAuthed = useSelector((state) => state.auth.isAuthed);
 
-    return(
-        <>
-        <Welcome data={data.Header} />
-        { isAuthed && <Job data={data.Features} /> }
-        <Subscribe data={data.Contact} />
-        </>
-    )
-}
+  return (
+    <>
+      <Welcome data={data.Header} />
+      {isAuthed && <Job data={data.Features} />}
+      <Subscribe data={data.Contact} />
+    </>
+  );
+};
 
 export default HomePage;
