7839dbd5eb79c685a325c0bf7c2ececaf9f91f71
cleandevcode <freelance1331@gmail.com>
2021-08-18 23:27:26 +0800

    update question page

--- src/pages/Job/job.jsx
+++ src/pages/Job/job.jsx
@@ -9,13 +9,15 @@ import {
   FormLabel,
   Dropdown,
   Form,
+  Alert
 } from "react-bootstrap";
 import { URLInput } from "../../components/inputs/url";
-import { JobOptions } from "../../constants";
+import { JobOptions, ReferOptions, TaskOptions, Questions } from "../../constants";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import { Routes } from "../../routes";
 import Profile from '../Profile/profile';
 import './job.scss';
+import { updateMisc } from "../../service/user.service";
 
 const Job = (props) => {
   const { history } = props;
@@ -26,24 +28,20 @@ const Job = (props) => {
     history.push({ pathname: Routes.Home.path });
   }
   const captchaToken = useSelector((state) => state.hmt.captchaToken);
-  const [option, setOptions] = useState(JobOptions.referal);
+  const [option, setOptions] = useState(JobOptions.questionare);
   const [referralCode, setReferralCode] = useState(user ? user.referralCode || '' : '');
-  const [captchaCnt, setCaptchaCnt] = useState(0);
-  const [referalCnt, setReferalCnt] = useState(0);
+  // const [captchaCnt, setCaptchaCnt] = useState(0);
+  // const [referalCnt, setReferalCnt] = useState(0);
   const [submitted, setSubmitted] = useState(false);
   const [nextable, setNextable] = useState(false);
   const [errorText, setErrorText] = useState("");
-  const hmtCounts = useSelector((state) => state.hmt.htmCounts);
-  const [radios, setRadios] = useState([
-    { checked: false, value: "opt_1", label: "Option 1", name: "question_4" },
-    { checked: false, value: "opt_2", label: "Option 2", name: "question_4" },
-    { checked: false, value: "opt_3", label: "Option 3", name: "question_4" },
-  ]);
-  const [optItems, setOptItems] = useState([
-    { value: "Option 1", checked: false },
-    { value: "Option 2", checked: false },
-    { value: "Option 3", checked: false },
-  ]);
+  const [currentQuestion, setCurrentQuestion] = useState('task');
+  // const hmtCounts = useSelector((state) => state.hmt.htmCounts);
+  const [taskOptions, setTaskOptions] = useState([]);
+  const [referOptions, setReferOptions] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [refers, setRefers] = useState('');
+  const [ otherQuestion, setOtherQuestion ] = useState('');
   const [questions, setQuestions] = useState({
     question_1: true
   });
@@ -52,18 +50,39 @@ const Job = (props) => {
     if(history.location.state && history.location.state.jobOption) {
       setOptions(history.location.state.jobOption);
     }
+
+    let taskOptions = TaskOptions.map((taskOpt) => ({ checked: false, ...taskOpt }));
+    setTaskOptions(taskOptions);
+
+    let referOptions = ReferOptions.map((refereOpt) => ({ checked: false, ...refereOpt }));
+    setReferOptions(referOptions);
   }, [])
 
   const submitQuestions = (e) => {
     e.preventDefault();
-    setSubmitted(true);
-    if (
-      questions.question_1 &&
-      questions.question_2 &&
-      questions.question_3 &&
-      questions.question_4
-    ) {
-      history.push({ pathname: Routes.Profile.path });
+    if(!refers) 
+      return setErrorText('Answer required');
+    else {
+      const questions = [
+        {
+          q: Questions.task,
+          a: tasks
+        },
+        {
+          q: Questions.refer,
+          a: refers
+        }
+      ];
+      return updateMisc(user.id, token, questions)
+        .then((response) => {
+          if(response) {
+            dispatch({ type: 'SET_USER', payload: response });
+            setErrorText('');
+            history.push({ pathname: Routes.Home.path });
+          } else {
+            setErrorText('Failed to submit questions');
+          }
+        }).catch((err) => setErrorText(err.message));
     }
   };
 
@@ -73,20 +92,35 @@ const Job = (props) => {
       case 'referal':
         setReferralCode(value);
         break;
+      case 'otherQuestion':
+        setOtherQuestion(value);
+        if(currentQuestion === 'refer') {
+          let newReferOpts = referOptions.map((Opt) => {
+            Opt.checked = false;
+            return Opt;
+          });
+          setReferOptions(newReferOpts);
+          setRefers(value);
+        }
+        break;
+      case 'task':
+        const newTaskOpts = taskOptions.map((taskOpt) => {
+          if(taskOpt.value === value) taskOpt.checked = true;
+          return taskOpt;
+        })
+        setTaskOptions(newTaskOpts);
+        break;
+      case 'refer':
+        let newReferOpts = referOptions.map((Opt) => {
+          if(Opt.value === value) Opt.checked = true;
+          else Opt.checked = false;
+          return Opt;
+        });
+        setReferOptions(newReferOpts);
+        setRefers(value);
+        break;
       default:
         break;
-    }
-    if (name === "question_4") {
-      const newRadios = radios.map((radio) => {
-        if (radio.value === value) {
-          radio.checked = true;
-          setQuestions((questions) => ({ ...questions, [name]: radio.label }));
-        }
-        return radio;
-      });
-      setRadios(newRadios);
-    } else {
-      setQuestions((questions) => ({ ...questions, [name]: value }));
     }
   };
 
@@ -107,19 +141,33 @@ const Job = (props) => {
     });
   };
 
-  const handleQuestions = (e) => {
+  const handleNext = (e) => {
     e.preventDefault();
-    console.log({ name: e.target.name, value: e.target.value });
-  }
-
-  const handleNext = () => {
-    if (captchaToken && captchaToken.length > 0) {
-      setErrorText("");
-      setNextable(true);
-    } else {
-      setErrorText("You need to pass captCha.");
-      setNextable(false);
+    switch (option) {
+      case JobOptions.questionare:
+        const tasks = otherQuestion ? [otherQuestion] : [];
+        taskOptions.map((taskOption) => {
+          if(taskOption.checked) tasks.push(taskOption.value);
+        });
+        if(tasks.length) {
+          setErrorText('');
+          setOtherQuestion('');
+          setTasks(tasks);
+          setCurrentQuestion('refer');
+        } else {
+          setErrorText('Answer required')
+        }
+        break;
+      default:
+        break;
     }
+    // if (captchaToken && captchaToken.length > 0) {
+    //   setErrorText("");
+    //   setNextable(true);
+    // } else {
+    //   setErrorText("You need to pass captCha.");
+    //   setNextable(false);
+    // }
   };
 
   return (
@@ -141,38 +189,27 @@ const Job = (props) => {
                 </a>
               </li> */}
               <li className="">
-                <a
-                  className={`opt ${
-                    option && option === JobOptions.referal ? "active" : ""
-                  }`}
-                  onClick={() => setOptions(JobOptions.referal)}
-                >
-                  Referal
-                </a>
+                <a className={`opt ${ option && option === JobOptions.questionare ? "active" : "" }`} onClick={() => setOptions(JobOptions.questionare)}>Questionnaire</a>
               </li>
               <li className="">
-                <a
-                  className={`opt ${
-                    option && option === JobOptions.questionare ? "active" : ""
-                  }`}
-                  onClick={() => setOptions(JobOptions.questionare)}
-                >
-                  Questionare
-                </a>
+                <a className={`opt ${ option && option === JobOptions.profile ? "active" : "" }`} onClick={() => setOptions(JobOptions.profile)}>profile</a>
               </li>
               <li className="">
-                <a
-                  className={`opt ${
-                    option && option === JobOptions.profile ? "active" : ""
-                  }`}
-                  onClick={() => setOptions(JobOptions.profile)}
-                >
-                  profile
-                </a>
+                <a className={`opt ${ option && option === JobOptions.referal ? "active" : "" }`} onClick={() => setOptions(JobOptions.referal)}>Referal</a>
               </li>
             </ul>
           </div>
           <div className="col-md-6 section-content col-sm-12">
+            { user && !user.walletAddr &&
+            <Alert variant="primary">
+              <p>Please setup your wallet address in the Profile page.</p>
+            </Alert>
+            }
+            { user && user.walletAddr && !user.isKYCed &&
+            <Alert variant="primary">
+              <p>Pending KYC. Note: You won't be able to receive HMT until KYC is completed.</p>
+            </Alert>
+            }
             {option && option === JobOptions.captcha && (
               <div id='hcaptcha'>
                 <p className='d-none d-md-block'>For every hCaptcha puzzle you solve, you will earn around 0.01 - 0.1 HMT.</p>
@@ -203,7 +240,7 @@ const Job = (props) => {
                 <FormControl.Feedback
                   type="invalid"
                   className={
-                    submitted && !questions.referal ? "d-block text-left" : ""
+                    submitted && !referralCode ? "d-block text-left" : ""
                   }
                 >
                   Referal link required
@@ -223,24 +260,48 @@ const Job = (props) => {
               >
                 <p className='d-none d-md-block'>Complete the questionnaire to receive 1 HMT.</p>
                 <div className='question-list'>
-                  <p>What tasks would you prefer to do on the HUMAN App?</p>
+                  <p>{Questions[currentQuestion]}</p>
+                  { errorText && 
+                  <Alert variant="danger" onClose={() => setErrorText('')} dismissible>
+                    <p>{errorText}</p>
+                  </Alert>
+                  }
+                  { currentQuestion === 'task' &&
                   <Form name="form">
                     <FormGroup>
-                      <Form.Check type="checkbox" label="Solve captchas" />
-                      <Form.Check type="checkbox" label="Provide feedback on A/B tests." />
-                      <Form.Check type="checkbox" label="Code review and Bug bounties." />
-                      <Form.Check type="checkbox" label="Market research surveys." />
-                      <Form.Check type="checkbox" label="Data labelling on video and/or text." />
-                      <Form.Check type="checkbox" label="Partake in predictions markets." />
+                      { taskOptions && taskOptions.length && taskOptions.map((taskOpt, index) => 
+                        <Form.Check name='task' key={index} type="checkbox" label={taskOpt.label} checked={taskOpt.checked} value={taskOpt.value} onChange={handleChange}/>
+                      )}
                     </FormGroup>
                     <FormGroup className='other-question'>
-                        <FormControl className='m-0'></FormControl>
+                        <FormControl className='m-0' name='otherQuestion' value={otherQuestion} type='text' onChange={handleChange} />
                     </FormGroup>
                   </Form>
+                  }
+                  { currentQuestion === 'refer' &&
+                  <Form name="form">
+                    <FormGroup>
+                      { referOptions && referOptions.length && referOptions.map((referOpt, index) => 
+                        <Form.Check name='refer' key={index} type="radio" label={referOpt.label} checked={referOpt.checked} value={referOpt.value} onChange={handleChange}/>
+                      )}
+                    </FormGroup>
+                    <FormGroup className='other-question'>
+                        <FormControl className='m-0' name='otherQuestion' value={otherQuestion} type='text' onChange={handleChange} />
+                    </FormGroup>
+                  </Form>
+                  }
                 </div>
+                { currentQuestion === 'task' &&
                 <FormGroup>
-                  <Button className='form-control'>Next</Button>
+                  <Button className='form-control' onClick={handleNext}>Next</Button>
                 </FormGroup>
+                }
+                { currentQuestion === 'refer' &&
+                <FormGroup>
+                  <Button className='form-control' onClick={submitQuestions}>Submit</Button>
+                  <Button className='form-control' onClick={() => { setOtherQuestion(''); setCurrentQuestion('task');}}>Back</Button>
+                </FormGroup>
+                }
               </div>
             )}
             {option && option === JobOptions.profile && 
