47b69f63757cecb5ec99b227df57872cc61b5051
cleandevcode <freelance1331@gmail.com>
2021-09-05 04:22:57 +0800

    update password component

--- src/components/inputs/password/password.jsx
+++ src/components/inputs/password/password.jsx
@@ -1,29 +1,25 @@
-import { useState } from "react"
-import { FormControl } from "react-bootstrap";
+import { useEffect, useState } from "react"
+import { FormControl, InputGroup } from "react-bootstrap";
 import './password.css';
 
 export const Password = (props) => {
     const [hidden, setHidden] = useState(true);
+    const [value, setValue] = useState(props.value);
+
     const ToogleShow = (e) => {
         e.preventDefault();
         setHidden(!hidden);
     }
+    
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        props.onChange(e);
+    }
+
     return (
-        <div className='form-group password'>
-            <div className='input-group'>
-                <FormControl placeholder={props.placeholder} type={hidden ? 'password' : 'text'} name={props.name} value={props.value} onChange={props.onChange}></FormControl>
-                <span onClick={ToogleShow} className='position-absolute d-flex flex-column justify-content-center h-100'><i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true"></i></span>
-            </div>
-            { props.submitted && !props.value &&
-            <FormControl.Feedback className='d-block'type='invalid'>
-                <div key={`field-error-password`} className="fieldError">Password is required</div>
-            </FormControl.Feedback>
-            }
-            { props.submitted && props.value && !props.confirm &&
-            <FormControl.Feedback className='d-block'type='invalid'>
-                <div key={`field-error-password`} className="fieldError">Password not same</div>
-            </FormControl.Feedback>
-            }
-        </div>
+        <InputGroup>
+            <FormControl placeholder={props.placeholder} type={hidden ? 'password' : 'text'} name={props.name} value={value} onChange={handleChange} onBlur={props.onBlur}></FormControl>
+            <span onClick={ToogleShow} className='position-absolute d-flex flex-column justify-content-center h-100'><i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true"></i></span>
+        </InputGroup>
     )
 }
