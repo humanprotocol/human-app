0f5e34c0b2a47c7fb121836ff0382ec4eb272764
cleandevcode <freelance1331@gmail.com>
2021-09-14 23:47:27 +0800

    update login api

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -80,6 +80,7 @@ export const signIn = async ({ email, password, hcaptchaToken }) => {
       const { user, tokens } = response.data;
       // eslint-disable-next-line no-undef
       localStorage.setItem('token', tokens.access.token);
+      localStorage.setItem('refreshToken', tokens.refresh.token);
       return { user, token: tokens.access.token, refreshToken: tokens.refresh.token };
     })
     .catch(err => {
