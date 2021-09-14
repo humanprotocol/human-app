bd33a84dc2d97801850f689190b64bf16e5fa0df
cleandevcode <freelance1331@gmail.com>
2021-09-14 19:12:23 +0800

    install formik package

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,15 @@ import ReactGA from 'react-ga4';
 import SmoothScroll from 'smooth-scroll';
 import { createBrowserHistory } from 'history';
 
-import Navigation from './components/navigation/navigation';
-import HomePage from './pages/Home/home';
-import LoginPage from './pages/Login/login';
-import RegisterPage from './pages/Login/register';
-import ForgotPasswordPage from './pages/Login/forgotPassword';
-import ProfilePage from './pages/Profile/profile';
-import LinkWalletPage from './pages/Wallet/link';
-import VerifyEmail from './pages/Login/verifyEmail';
+import Navigation from "./components/navigation/navigation";
+import HomePage from "./pages/Home/home";
+import LoginPage from "./pages/Login/login";
+import LoginTestPage from "./pages/Login/logintest";
+import RegisterPage from "./pages/Login/register";
+import ForgotPasswordPage from "./pages/Login/forgotPassword";
+import ProfilePage from "./pages/Profile/profile";
+import LinkWalletPage from "./pages/Wallet/link";
+import VerifyEmail from "./pages/Login/verifyEmail";
 
 import { Subscribe } from './pages/Home/subscribe';
 import { Routes } from './routes';
@@ -40,6 +41,7 @@ const App = () => {
         <Switch>
           <Route exact path={Routes.Home.path} component={HomePage} />
           <Route exact path={Routes.Login.path} component={LoginPage} />
+          <Route exact path='/logintest' component={LoginTestPage} />
           <Route exact path={Routes.Register.path} component={RegisterPage} />
           <Route exact path={Routes.Profile.path} component={ProfilePage} />
           <Route exact path={Routes.LinkWallet.path} component={LinkWalletPage} />
--- src/components/inputs/password/password.jsx
+++ src/components/inputs/password/password.jsx
@@ -1,53 +1,17 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { FormControl } from 'react-bootstrap';
+import { useState } from "react"
+import { FormControl, InputGroup } from "react-bootstrap";
 import './password.css';
 
-export const Password = ({ placeholder, name, value, onChange, submitted, confirm }) => {
-  const [hidden, setHidden] = useState(true);
-  const ToogleShow = e => {
-    e.preventDefault();
-    setHidden(!hidden);
-  };
-  return (
-    <div className="form-group password">
-      <div className="input-group">
-        <FormControl
-          placeholder={placeholder}
-          type={hidden ? 'password' : 'text'}
-          name={name}
-          value={value}
-          onChange={onChange}
-        />
-        <span
-          onClick={ToogleShow}
-          className="position-absolute d-flex flex-column justify-content-center h-100"
-        >
-          <i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true" />
-        </span>
-      </div>
-      {submitted && !value && (
-        <FormControl.Feedback className="d-block" type="invalid">
-          <div key="field-error-password" className="fieldError">
-            Password is required
-          </div>
-        </FormControl.Feedback>
-      )}
-      {submitted && value && !confirm && (
-        <FormControl.Feedback className="d-block" type="invalid">
-          <div key="field-error-password" className="fieldError">
-            Password not same
-          </div>
-        </FormControl.Feedback>
-      )}
-    </div>
-  );
-};
-Password.propTypes = {
-  placeholder: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  submitted: PropTypes.bool.isRequired,
-  confirm: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
+export const Password = (props) => {
+    const [hidden, setHidden] = useState(true);
+    const ToogleShow = (e) => {
+        e.preventDefault();
+        setHidden(!hidden);
+    }
+    return (
+        <InputGroup>
+            <FormControl placeholder={props.placeholder} type={hidden ? 'password' : 'text'} name={props.name} value={props.value} onChange={props.onChange} onBlur={props.onBlur}></FormControl>
+            <span onClick={ToogleShow} className='position-absolute d-flex flex-column justify-content-center h-100'><i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true"></i></span>
+        </InputGroup>
+    )
+}
