035b5c07af29e7147f0b8b77d73e30910929e8ec
cleandevcode <freelance1331@gmail.com>
2021-09-14 19:12:23 +0800

    install formik package

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { createBrowserHistory } from "history";
 import Navigation from "./components/navigation/navigation";
 import HomePage from "./pages/Home/home";
 import LoginPage from "./pages/Login/login";
+import LoginTestPage from "./pages/Login/logintest";
 import RegisterPage from "./pages/Login/register";
 import ForgotPasswordPage from "./pages/Login/forgotPassword";
 import ProfilePage from "./pages/Profile/profile";
@@ -45,6 +46,7 @@ const App = () => {
         <Switch>
           <Route exact path={Routes.Home.path} component={HomePage} />
           <Route exact path={Routes.Login.path} component={LoginPage} />
+          <Route exact path='/logintest' component={LoginTestPage} />
           <Route exact path={Routes.Register.path} component={RegisterPage} />
           <Route exact path={Routes.Profile.path} component={ProfilePage} />
           <Route
--- src/components/inputs/password/password.jsx
+++ src/components/inputs/password/password.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FormControl } from "react-bootstrap";
+import { FormControl, InputGroup } from "react-bootstrap";
 import './password.css';
 
 export const Password = (props) => {
@@ -9,21 +9,9 @@ export const Password = (props) => {
         setHidden(!hidden);
     }
     return (
-        <div className='form-group password'>
-            <div className='input-group'>
-                <FormControl placeholder={props.placeholder} type={hidden ? 'password' : 'text'} name={props.name} value={props.value} onChange={props.onChange}></FormControl>
-                <span onClick={ToogleShow} className='position-absolute d-flex flex-column justify-content-center h-100'><i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true"></i></span>
-            </div>
-            { props.submitted && !props.value &&
-            <FormControl.Feedback className='d-block'type='invalid'>
-                <div key={`field-error-password`} className="fieldError">Password is required</div>
-            </FormControl.Feedback>
-            }
-            { props.submitted && props.value && !props.confirm &&
-            <FormControl.Feedback className='d-block'type='invalid'>
-                <div key={`field-error-password`} className="fieldError">Password not same</div>
-            </FormControl.Feedback>
-            }
-        </div>
+        <InputGroup>
+            <FormControl placeholder={props.placeholder} type={hidden ? 'password' : 'text'} name={props.name} value={props.value} onChange={props.onChange} onBlur={props.onBlur}></FormControl>
+            <span onClick={ToogleShow} className='position-absolute d-flex flex-column justify-content-center h-100'><i className={`fa ${hidden ? 'fa-eye-slash' : 'fa-eye'}`} aria-hidden="true"></i></span>
+        </InputGroup>
     )
 }
