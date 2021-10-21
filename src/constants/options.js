455d9a5242896ace51e8db13ddb93ef753af7f23
vkomodey <vkomodey@gmail.com>
2021-10-21 16:49:10 +0200

    Move options to constants.js

--- src/constants/options.js
+++ src/constants/options.js
@@ -20,3 +20,20 @@ export const jobOptions = {
   profile: 'profile',
   dataLabel: 'dataLabel',
 };
+
+export const referOptions = [
+  { value: 'Twitter', label: 'Twitter' },
+  { value: 'Telegram groups', label: 'Telegram groups' },
+  { value: 'Reddit', label: 'Reddit' },
+  { value: 'Google Search', label: 'Google Search' },
+  { value: 'Crypto news website', label: 'Crypto news website' },
+];
+
+export const taskOptions = [
+  { label: 'Solve captchas', value: 'Solve captchas' },
+  { label: 'Provide feedback on A/B tests.', value: 'Provide feedback on A/B tests.' },
+  { label: 'Code review and Bug bounties.', value: 'Code review and Bug bounties.' },
+  { label: 'Market research surveys.', value: 'Market research surveys.' },
+  { label: 'Partake in predictions markets.', value: 'Partake in predictions markets.' },
+  { label: 'Data labelling on video and/or text.', value: 'Data labelling on video and/or text.' },
+];
