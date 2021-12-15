7ae209615ba1b6f6b721779cd22fc86002e27779
Vlad Komodey <vkomodey@gmail.com>
2021-12-15 18:26:03 +0100

    Update wordings for polygon and withdrawals alerts

--- src/components/alert/wallet.jsx
+++ src/components/alert/wallet.jsx
@@ -5,9 +5,9 @@ export function SetupWalletAlert() {
   return (
     <Alert variant="primary">
       <p>
-        We switched to the Polygon network. Please, provide your wallet address in the Polygon
-        Mainnet. If you have any pending withdrawals in the Ethereum Mainnet, they will be
-        automatically converted to the Polygon with updated wallet address.
+        We have switched to the Polygon network. You'll need to replace your Ethereum wallet address
+        with one connected to Polygon. Any pending withdrawals will be automatically converted once
+        the wallet address is updated.
       </p>
     </Alert>
   );
