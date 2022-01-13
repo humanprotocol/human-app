4a11ffca22cfed709f9e5fad8f085ee037538596
vkomodey <vkomodey@gmail.com>
2022-01-13 21:25:05 +0100

    Add temporary verification promotion alert with 1 HMT as a reward

--- src/pages/Workspace/alerts.jsx
+++ src/pages/Workspace/alerts.jsx
@@ -12,3 +12,13 @@ export function SetupWalletAlert() {
     </Alert>
   );
 }
+
+export function VerificationPromotionMessage() {
+  return (
+    <Alert variant="primary">
+      <p>
+        Pass the verification in the Civic App before January 17, 09:00 AM UTC and get 1 free HMT!
+      </p>
+    </Alert>
+  );
+}
