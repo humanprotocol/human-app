65ce740538c8105c833c2e4635e306509fce7429
Ivan <ivan@hmt.ai>
2022-05-13 21:38:47 -0300

    nice-to-have-but-not-a-must improvements of deploy script just to check the CI triggering

--- .github/workflows/common/deploy.bash
+++ .github/workflows/common/deploy.bash
@@ -1,30 +1,32 @@
 #!/usr/bin/env bash
 
 set -x # verbose
-set -e # quit on error
+set -e # quit on error with last exit code
 set -eo pipefail
 
 readonly REGISTRY="340792883311.dkr.ecr.us-east-2.amazonaws.com"
 readonly IMAGE="$1"
 readonly CONTAINER_NAME="human-app-ui"
 
-docker pull $IMAGE
+docker pull $IMAGE || echo "Failed to pull docker image $IMAGE"
 
-if [ $? -eq 0 ]; then
-  echo "Stopping frontend container"
-  docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
-
-  echo "Runing new frontend container"
-  docker run \
-    --name=$CONTAINER_NAME \
-    -d \
-    -p 80:8080 \
-    -e PORT=8080 \
-    -e ENABLE_CSP_FF=true \
-    --network=human-protocol-staging \
-    --restart=on-failure \
-    $IMAGE
-else
-  echo "Failed to pull docker image"
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+	echo "Stopping frontend container"
+	docker stop $CONTAINER_NAME
 fi
+
+if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+	# cleanup
+	docker rm $CONTAINER_NAME
+fi
+
+echo "Runing new frontend container: $IMAGE"
+docker run \
+--name=$CONTAINER_NAME \
+-d \
+-p 80:8080 \
+-e PORT=8080 \
+-e ENABLE_CSP_FF=true \
+--network=human-protocol-staging \
+--restart=on-failure \
+$IMAGE
