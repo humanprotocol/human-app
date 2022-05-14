c32bc78191d18f3f3c6a4d373e88c5eda9b4f5a1
makarofalex3 <alek@hmt.ai>
2022-05-14 22:36:06 +0300

    removed abi to artifact folder, added craco to package json

--- src/hooks/useContractAbi.js
+++ src/hooks/useContractAbi.js
@@ -1,19 +1,18 @@
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import HMTokenABI from '../abi/HMTokenABI.json';
+import HMTokenABI from '../../artifacts/HMTokenABI.json';
 import notifier from '../service/notify.service';
 
-const PROVIDER_URL = 'https://polygon-mainnet.infura.io/v3/8bf66d194c344a65adc7018e25646061';
-const HUMAN_HMT_TOKEN_CONTRACT_ADDRESS = '0x444c45937D2202118a0FF9c48d491cef527b59dF';
+const providerUrl = process.env.REACT_APP_POLYGON_MAINNET;
+const tokenArrd = process.env.REACT_APP_HUMAN_HMT_TOKEN_CONTRACT_ADDRESS;
 
 export const useContractAbi = ({ walletAddr }) => {
   const [balance, setBalance] = useState();
-
   useEffect(() => {
     (async () => {
       try {
-        const Web3Client = new Web3(PROVIDER_URL);
-        const contract = new Web3Client.eth.Contract(HMTokenABI, HUMAN_HMT_TOKEN_CONTRACT_ADDRESS);
+        const Web3Client = new Web3(providerUrl);
+        const contract = new Web3Client.eth.Contract(HMTokenABI, tokenArrd);
         const weiValue = await contract.methods.balanceOf(walletAddr).call();
         const tokenBalance = Web3Client.utils.fromWei(weiValue, 'ether');
         setBalance(tokenBalance);
