474444f46a30cdd5ecebd1d85631ce9b1e463446
makarofalex3 <alek@hmt.ai>
2022-05-16 21:11:13 +0300

    removed craco, replaced abi

--- src/hooks/useContractAbi.js
+++ src/hooks/useContractAbi.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import HMTokenABI from '../../artifacts/HMTokenABI.json';
+import HMTokenABI from '../artifacts/HMTokenABI.json';
 import notifier from '../service/notify.service';
 
 const providerUrl = process.env.REACT_APP_POLYGON_MAINNET;
-const tokenArrd = process.env.REACT_APP_HUMAN_HMT_TOKEN_CONTRACT_ADDRESS;
+const tokenAddr = process.env.REACT_APP_HUMAN_HMT_TOKEN_CONTRACT_ADDRESS;
 
 export const useContractAbi = ({ walletAddr }) => {
   const [balance, setBalance] = useState('');
@@ -12,7 +12,7 @@ export const useContractAbi = ({ walletAddr }) => {
     (async () => {
       try {
         const Web3Client = new Web3(providerUrl);
-        const contract = new Web3Client.eth.Contract(HMTokenABI, tokenArrd);
+        const contract = new Web3Client.eth.Contract(HMTokenABI, tokenAddr);
         const weiValue = await contract.methods.balanceOf(walletAddr).call();
         const tokenBalance = Web3Client.utils.fromWei(weiValue, 'ether');
         setBalance(tokenBalance);
